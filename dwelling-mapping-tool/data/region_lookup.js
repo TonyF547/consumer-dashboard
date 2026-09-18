fb256f4f057d0f431e4af39208ec65b798822150
TonyF547 <tfeiertag@earthcare.co.nz>
2026-09-18 18:52:05 +1200

    Add files via upload

---
+++ dwelling-mapping-tool/data/region_lookup.js
@@ -0,0 +1,103 @@
+window.REGION_TLAS = {
+  "Northland": [
+    "Far North District",
+    "Whangarei District",
+    "Kaipara District"
+  ],
+  "Auckland": [
+    "Auckland"
+  ],
+  "Waikato": [
+    "Thames-Coromandel District",
+    "Hauraki District",
+    "Waikato District",
+    "Matamata-Piako District",
+    "Hamilton City",
+    "Waipa District",
+    "Ōtorohanga District",
+    "South Waikato District",
+    "Waitomo District",
+    "Taupo District"
+  ],
+  "Bay of Plenty": [
+    "Western Bay of Plenty District",
+    "Tauranga City",
+    "Rotorua District",
+    "Whakatane District",
+    "Kawerau District",
+    "Ōpōtiki District"
+  ],
+  "Gisborne": [
+    "Gisborne District"
+  ],
+  "Hawke's Bay": [
+    "Wairoa District",
+    "Hastings District",
+    "Napier City",
+    "Central Hawke's Bay District"
+  ],
+  "Taranaki": [
+    "New Plymouth District",
+    "Stratford District",
+    "South Taranaki District"
+  ],
+  "Manawatu-Whanganui": [
+    "Ruapehu District",
+    "Whanganui District",
+    "Rangitikei District",
+    "Manawatu District",
+    "Palmerston North City",
+    "Tararua District",
+    "Horowhenua District"
+  ],
+  "Wellington": [
+    "Masterton District",
+    "Carterton District",
+    "South Wairarapa District",
+    "Kapiti Coast District",
+    "Porirua City",
+    "Upper Hutt City",
+    "Lower Hutt City",
+    "Wellington City"
+  ],
+  "Tasman": [
+    "Tasman District"
+  ],
+  "Nelson": [
+    "Nelson City"
+  ],
+  "Marlborough": [
+    "Marlborough District"
+  ],
+  "West Coast": [
+    "Buller District",
+    "Grey District",
+    "Westland District"
+  ],
+  "Canterbury": [
+    "Kaikoura District",
+    "Hurunui District",
+    "Waimakariri District",
+    "Christchurch City",
+    "Selwyn District",
+    "Ashburton District",
+    "Timaru District",
+    "Mackenzie District",
+    "Waimate District"
+  ],
+  "Otago": [
+    "Waitaki District",
+    "Central Otago District",
+    "Queenstown-Lakes District",
+    "Dunedin City",
+    "Clutha District"
+  ],
+  "Southland": [
+    "Southland District",
+    "Gore District",
+    "Invercargill City"
+  ],
+  "Chatham Islands": [
+    "Chatham Islands Territory"
+  ]
+};
